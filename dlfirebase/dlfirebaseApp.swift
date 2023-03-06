965b29132c109d7d2cd406393533bcc5ef5b914d
Abuzar <57677552+abuzariftikhar@users.noreply.github.com>
2023-03-07 00:17:05 +0500

    init

---
+++ dlfirebase/dlfirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  dlfirebaseApp.swift
+//  dlfirebase
+//
+//  Created by Abuzar on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct dlfirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
